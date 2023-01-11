86b56aad8d3cdfaef0406b6998cd30ad8529eb7a
Edison <735318617@qq.com>
2023-01-11 13:06:41 +0800

    add denoise

--- Main.py
+++ Main.py
@@ -46,7 +46,10 @@ if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(device)
 
+    # Use CNN
     myModel = IMUClassifier()
+    # Use ResNet
+    # myModel = resnet18()
     myModel = myModel.to(device)
 
     # Training Model
